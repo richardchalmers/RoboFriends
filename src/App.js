cfe160aa9bf62a5ba3b368a21f988ba518a164a8
RichardC32 <richard.chalmersseo@gmail.com>
2018-09-18 16:47:49 +1000

    fixed header added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 import './App.css';
-
+import Scroll from './Scroll';
 class App extends Component {
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
 	}
+
+	componentDidMount(){
+		fetch('http://jsonplaceholder.typicode.com/users')
+		.then(response=> response.json())
+		.then(users=> this.setState({ robots: users}));
+	} 
+			
+	
 
 	onSearchChange = (event) => {
 		this.setState({searchfield: event.target.value})
@@ -24,7 +31,9 @@ class App extends Component {
 		<div className = 'tc'>
 			<h1 className ='f1'>RoboFriends</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
-			<CardList robots={filteredRobots}/>
+			<Scroll>
+				<CardList robots={filteredRobots}/>
+			</Scroll>
 		</div>
 	);
 	}

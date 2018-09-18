acd0434ebf40aee079abb88ac83babb1f737f9e5
RichardC32 <richard.chalmersseo@gmail.com>
2018-09-18 15:13:55 +1000

    dynamic searchbox

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
 
+class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
 
-const App = () => {
-	return (
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value})
+	}
+	render(){
+		const filteredRobots = this.state.robots.filter(robots =>{
+		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
 		<div className = 'tc'>
 			<h1>RoboFriends</h1>
-			<SearchBox />
-			<CardList robots={robots}/>
+			<SearchBox searchChange={this.onSearchChange}/>
+			<CardList robots={filteredRobots}/>
 		</div>
-	)
+	);
+	}
+	
 }
 export default App;
